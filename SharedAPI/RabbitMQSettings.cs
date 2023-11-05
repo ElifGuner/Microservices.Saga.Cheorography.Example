faf33ffb36954fe24bd864e21dcbc951faf054ce
Huseyin Guner <huseyin_guner@hotmail.com.tr>
2023-11-06 01:10:44 +0300

    new changes

---
+++ SharedAPI/RabbitMQSettings.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+static public class RabbitMQSettings
+{
+    public const string Stock_OrderCreatedEventQueue = "stock-order-created-event-queue";
+}
--- StockAPI/Models/StockAPIDbContext.cs
+++ StockAPI/Models/StockAPIDbContext.cs
@@ -4,5 +4,20 @@ namespace StockAPI.Models
 {
     public class StockAPIDbContext : DbContext
     {
-    }
+        public StockAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public Dbset<Stock> Stocks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Stock>().HasData(
+             new Stock() { Id = 1, ProductId = Guid.NewGuid(), Count = 2000 },
+             new Stock() { Id = 2, ProductId = Guid.NewGuid(), Count = 1000 },
+             new Stock() { Id = 3, ProductId = Guid.NewGuid(), Count = 3000 },
+             new Stock() { Id = 4, ProductId = Guid.NewGuid(), Count = 5000 },
+             new Stock() { Id = 5, ProductId = Guid.NewGuid(), Count = 500 }
+            );
+        }
 }
